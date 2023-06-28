d14a2777344d52191130d0e7ad1059f5d8af86a0
PetrAlexkulakov <petr.alex.kulakov@gmail.com>
2023-06-28 16:34:31 +0400

    feat: create users get

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,41 @@
 import http from 'http';
 import { v4 as uuidv4 } from 'uuid';
+import usersBase from './users.json' assert { type: 'json' };
 
-const server = http.createServer((req, res) => {});
+interface User {
+  id: string;
+  username: string;
+  age: number;
+  hobbies: string[];
+}
 
-const port = process.env.PORT || 3000;
+const users: User[] = usersBase; //or you can type anything else
+
+const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  if (url === '/api/users' && method === 'GET') {
+    // Получение всех пользователей
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode = 200;
+    res.end(JSON.stringify(users));
+  } else if (url !== undefined && url.startsWith('/api/users/') && method === 'GET') {
+    // Получение пользователя по ID
+    const userId = url.split('/')[3];
+    const user = users.find((u) => u.id === userId);
+
+    if (user) {
+      res.setHeader('Content-Type', 'application/json');
+      res.statusCode = 200;
+      res.end(JSON.stringify(user));
+    } else {
+      res.statusCode = 404;
+      res.end('User not found');
+    }
+  } 
+});
+
+const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
